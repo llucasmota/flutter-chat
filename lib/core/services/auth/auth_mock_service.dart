099fc2053c1b71b246aef8fa52ae3dde25581173
llucasmota <lucas.o.mota@gmail.com>
2023-05-22 08:01:55 -0300

    feat(authService): criação de classe abstrata de Auth Service e implementação de mock da mesma

---
+++ lib/core/services/auth/auth_mock_service.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+
+import 'package:chat/core/models/chat_user.dart';
+import 'dart:io';
+import 'dart:async';
+import 'package:chat/core/services/auth/auth_service.dart';
+
+class AuthMockService implements AuthService {
+  static Map<String, ChatUser> _users = {};
+  static ChatUser? _currentUser;
+
+  static MultiStreamController<ChatUser?>? _controller;
+
+  static final _userStream = Stream<ChatUser?>.multi((controller) {
+    _controller = controller;
+    _updateUser(null);
+  });
+
+  @override
+  // TODO: implement currentUser
+  ChatUser? get currentUser => _currentUser;
+
+  @override
+  Future<void> login(String email, String password) async {
+    _updateUser(_users[email]);
+  }
+
+  @override
+  Future<void> logout() async {
+    _updateUser(null);
+  }
+
+  @override
+  Future<void> signup(
+      String name, String email, String password, File? image) async {
+    final newUser = ChatUser(
+        id: Random().nextDouble().toString(),
+        email: email,
+        name: name,
+        imageUrl: image?.path ?? '');
+
+    _users.putIfAbsent(email, () => newUser);
+    _updateUser(newUser);
+  }
+
+  @override
+  Stream<ChatUser?> get userChanges => _userStream;
+
+  static void _updateUser(ChatUser? user) {
+    _currentUser = user;
+    _controller?.add(_currentUser);
+  }
+}
