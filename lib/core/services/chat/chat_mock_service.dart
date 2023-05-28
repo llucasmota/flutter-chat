b9e6ead35da35e43e51722a0908c64a8bae6dace
llucasmota <lucas.o.mota@gmail.com>
2023-05-28 19:58:42 -0300

    feat(services/chat): criação de classe abastrata e implementação de chat_service

---
+++ lib/core/services/chat/chat_mock_service.dart
@@ -0,0 +1,50 @@
+import 'dart:math';
+
+import 'package:chat/core/models/chat_messages.dart';
+import 'package:chat/core/models/chat_user.dart';
+import 'package:chat/core/services/chat/chat_service.dart';
+import 'dart:async';
+
+class ChatMockService implements ChatService {
+  static final List<ChatMessage> _msgs = [
+    ChatMessage(
+        id: '1',
+        text: 'Bom dia',
+        createdAt: DateTime.now(),
+        userId: '1',
+        userName: 'John Doe',
+        userImageURL: 'assets/images/avatar.png'),
+    ChatMessage(
+        id: '2',
+        text: 'Fala, parça',
+        createdAt: DateTime.now(),
+        userId: '1',
+        userName: 'Fulano',
+        userImageURL: 'assets/images/avatar.png')
+  ];
+  static MultiStreamController<List<ChatMessage>>? _controller;
+  static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
+    _controller = controller;
+  });
+  @override
+  Stream<List<ChatMessage>> messageStream() {
+    // TODO: implement messageStream
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ChatMessage> save(String text, ChatUser user) async {
+    final newMessage = ChatMessage(
+        id: Random().nextDouble().toString(),
+        text: text,
+        createdAt: DateTime.now(),
+        userId: user.id,
+        userName: user.name,
+        userImageURL: user.imageUrl);
+    _msgs.add(newMessage);
+
+    _controller?.add(_msgs);
+
+    return newMessage;
+  }
+}
