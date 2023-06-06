47eb658a627522c9e0b8332b92ea8b12e46b4a8a
llucasmota <lucas.o.mota@gmail.com>
2023-06-06 20:48:28 -0300

    feat(chat_mock): comentando mensagens fakes

--- lib/core/services/chat/chat_mock_service.dart
+++ lib/core/services/chat/chat_mock_service.dart
@@ -7,29 +7,43 @@ import 'dart:async';
 
 class ChatMockService implements ChatService {
   static final List<ChatMessage> _msgs = [
-    ChatMessage(
-        id: '1',
-        text: 'Bom dia',
-        createdAt: DateTime.now(),
-        userId: '1',
-        userName: 'John Doe',
-        userImageURL: 'assets/images/avatar.png'),
-    ChatMessage(
-        id: '2',
-        text: 'Fala, parça',
-        createdAt: DateTime.now(),
-        userId: '1',
-        userName: 'Fulano',
-        userImageURL: 'assets/images/avatar.png')
+    // ChatMessage(
+    //     id: '1',
+    //     text: 'Bom dia',
+    //     createdAt: DateTime.now(),
+    //     userId: '1',
+    //     userName: 'John Doe',
+    //     userImageURL: 'assets/images/avatar.png'),
+    // ChatMessage(
+    //     id: '2',
+    //     text: 'Fala, parça',
+    //     createdAt: DateTime.now(),
+    //     userId: '2',
+    //     userName: 'Atendente',
+    //     userImageURL: 'assets/images/avatar.png'),
+    // ChatMessage(
+    //     id: '3',
+    //     text: 'Em que posso ajudar???',
+    //     createdAt: DateTime.now(),
+    //     userId: '2',
+    //     userName: 'Atendente',
+    //     userImageURL: 'assets/images/avatar.png'),
+    // ChatMessage(
+    //     id: '4',
+    //     text: 'Me dê dinheilo',
+    //     createdAt: DateTime.now(),
+    //     userId: '1',
+    //     userName: 'John Doe',
+    //     userImageURL: 'assets/images/avatar.png'),
   ];
   static MultiStreamController<List<ChatMessage>>? _controller;
   static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
     _controller = controller;
+    controller.add(_msgs);
   });
   @override
   Stream<List<ChatMessage>> messageStream() {
-    // TODO: implement messageStream
-    throw UnimplementedError();
+    return _msgsStream;
   }
 
   @override
@@ -42,8 +56,7 @@ class ChatMockService implements ChatService {
         userName: user.name,
         userImageURL: user.imageUrl);
     _msgs.add(newMessage);
-
-    _controller?.add(_msgs);
+    _controller?.add(_msgs.reversed.toList());
 
     return newMessage;
   }
