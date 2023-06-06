f7767b56d40288249e1e949ba799f84bcc4d784a
llucasmota <lucas.o.mota@gmail.com>
2023-06-06 20:50:50 -0300

    feat(new_messages): adicionando streamBuilder

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,3 +1,7 @@
+import 'package:chat/components/messages_bubble.dart';
+import 'package:chat/core/models/chat_messages.dart';
+import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:chat/core/services/chat/chat_mock_service.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
@@ -5,8 +9,34 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Mensagens'),
+    final currentUser = AuthService().currentUser;
+
+    return StreamBuilder<List<ChatMessage>>(
+      stream: ChatMockService().messageStream(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return const Center(
+            child: Text('Sem mensagens. Vamos conversar?'),
+          );
+        } else {
+          final msgs = snapshot.data!;
+          return ListView.builder(
+            reverse: true,
+            itemCount: msgs.length,
+            itemBuilder: (ctx, index) {
+              return MessagesBubble(
+                  key: ValueKey(msgs[index].id),
+                  message: msgs[index],
+                  belongsToCurrentUser: currentUser?.id == msgs[index].userId);
+            },
+          );
+        }
+      },
     );
   }
 }
