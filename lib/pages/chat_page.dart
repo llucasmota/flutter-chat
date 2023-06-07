32416c5cc2acc7a56f01e118c69a0f939c56b760
llucasmota <lucas.o.mota@gmail.com>
2023-06-07 07:52:01 -0300

    feat(chat_page): adicionando ícone de notifificações, bem como action para o mesmo

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,7 +1,10 @@
 import 'package:chat/components/messages.dart';
 import 'package:chat/components/new_message.dart';
 import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:chat/core/services/notification/chat_notification_service.dart';
+import 'package:chat/pages/notification_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -9,11 +12,11 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Cod3r Chat'),
-          actions: [
-            DropdownButton(
-              padding: const EdgeInsets.only(top: 10),
+      appBar: AppBar(
+        title: const Text('Cod3r Chat'),
+        actions: [
+          DropdownButtonHideUnderline(
+            child: DropdownButton(
               icon: Icon(
                 Icons.more_vert,
                 color: Theme.of(context).colorScheme.secondary,
@@ -43,15 +46,41 @@ class ChatPage extends StatelessWidget {
                 }
               },
             ),
+          ),
+          Stack(children: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (ctx) => const NotificationPage(),
+                ));
+              },
+              icon: const Icon(
+                Icons.notifications,
+              ),
+            ),
+            Positioned(
+              top: 5,
+              right: 5,
+              child: CircleAvatar(
+                backgroundColor: Colors.red.shade800,
+                maxRadius: 10,
+                child: Text(
+                  '${Provider.of<ChatNotificationService>(context).itemsCount}',
+                  style: const TextStyle(fontSize: 12),
+                ),
+              ),
+            )
+          ])
+        ],
+      ),
+      body: const SafeArea(
+        child: Column(
+          children: [
+            Expanded(child: Messages()),
+            NewMessages(),
           ],
         ),
-        body: const SafeArea(
-          child: Column(
-            children: [
-              Expanded(child: Messages()),
-              NewMessages(),
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
