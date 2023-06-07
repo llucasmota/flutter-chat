54713cf0d1d93dbdc2aacc362ac814ca9a4d2ac9
llucasmota <lucas.o.mota@gmail.com>
2023-06-07 07:52:23 -0300

    feat(notification_page): criação de página de notificações

---
+++ lib/pages/notification_page.dart
@@ -0,0 +1,29 @@
+import 'package:chat/core/services/notification/chat_notification_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final service = Provider.of<ChatNotificationService>(context);
+    final items = service.items;
+
+    return Scaffold(
+        appBar: AppBar(title: const Text('Minhas Notificações')),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(items[index].title),
+              subtitle: Text(items[index].body),
+              trailing: IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () => service.remove(index),
+              ),
+            );
+          },
+          itemCount: service.itemsCount,
+        ));
+  }
+}
